3ae504c4d5bd94cbb9d3dba3a7f56b3aef0bc6b6
Muhammad Kasyfi <kasyfikh@example.com>
2024-06-11 04:07:24 +0800

    ignore this

--- demo/src/main/java/demo_ver/demo/model/ManageRole.java
+++ demo/src/main/java/demo_ver/demo/model/ManageRole.java
@@ -17,11 +17,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
 @EntityScan
 @Entity
 @Table(name = "manage_role")
